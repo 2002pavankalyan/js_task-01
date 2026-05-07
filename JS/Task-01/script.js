de6679326d0d55f2569e6e471ecdb8546c95dcca
hima <obbu2002@gmail.com>
2026-05-07 14:36:51 +0530

    First commit

---
+++ JS/Task-01/script.js
@@ -0,0 +1,180 @@
+//1
+// var a = 10;
+// a = 15;
+// console.log(a);
+
+//2
+// let b = 20;
+// let b = 30;
+// console.log(b);
+
+//3
+// const c = 50;
+// console.log(c);
+
+//4
+// const x = 10;
+// x = 20;
+//TypeError: Assignment to constant variable.
+
+//5
+// var p = 5;
+// var p = 10;
+// console.log(p);
+
+//6
+// let q = 25;
+// q = q + 10;
+// console.log(q);
+
+//7
+// const r = 7;
+// console.log(r + 3);
+
+//8
+// let a = 10;
+// //or
+// var a = 10;
+// //or
+// const a = 10;
+// //or
+// let a = 10;
+
+//9
+//Which method prints normal output?
+//console.log();
+
+//10
+//Which method shows warning?
+//console.warn();
+
+//11
+//Which method shows error?
+//console.error();
+
+//12
+//What does this do? console.clear();
+//It clears the output in the browser developer tools or terminal console.
+
+//13
+// let a = "hello";
+// console.log(typeof a);
+//"a" is variable, "hello" is String that is in variable a. "console.log(typeof a)" will display the data type of a variable.
+
+//14
+// let b = 100;
+// console.log(typeof b);
+//"b" is variable, "100" is number that is in variable b. "console.log(typeof b)" will display the data type of a variable.
+
+//15
+// let c = false;
+// console.log(typeof c);
+//"c" is variable, "false" is boolean that is in variable c. "console.log(typeof c)" will display the data type of a variable.
+
+//16
+// let d;
+// console.log(d);
+//d is declared but no value is assigned.So JS gives it the value undefined.
+
+//17
+//What is the type of null?
+//The output is "object". Even through "null" is empty value.Because of some old Bug in JS that is still exists.
+
+//18
+//Convert string to number: "25"
+// Number("25")
+// console.log(typeof 25);
+
+//19
+// let fruits = ["Apple", "Banana", "Mango"];
+// console.log(fruits);
+
+//20
+// let arr = ["a","b","c"];
+// console.log(arr[1]);
+
+//21
+// let arr = ["x","y","z"];
+// console.log(arr[arr.length-1]);
+
+//22
+// let fruits = ["Apple", "Banana", "Mango"];
+// console.log(fruits[0]);
+
+//23
+// let arr = ["apple","banana"];
+// console.log(arr.length);
+
+//24
+// Add new element to array.
+// let fruits = ["Apple", "Banana", "Mango"];
+// fruits.push("Orange");
+// console.log(fruits);
+
+//25
+// let person = {
+//     name: "Pavan",
+//     age: 23
+// };
+// console.log(person);
+
+//26
+// let obj = {name:"Naveen"};
+// console.log(obj.name);
+
+//27
+// let obj = {
+//   fruits: ["apple","banana"]
+// };
+// console.log(obj.fruits[1]);
+
+//28
+// let data = {
+//     fruits: ["Apple", "Banana", "Mango"]
+// };
+// console.log(data.fruits[data.fruits.length - 1]);
+
+//29
+// console.log(5 + 3);
+
+//30
+// console.log(10 % 3);
+
+//31
+// console.log(2 ** 3);
+
+//32
+// console.log(10 / 2);
+
+//33
+// let a = 5;
+// a++;
+// console.log(a);
+
+//34
+// let b = 5;
+// let c = b++;
+// console.log(b, c);
+
+//35
+// let x = 5;
+// let y = ++x;
+// console.log(x, y);
+
+//36
+// let m = 3;
+// let n = m--;
+// console.log(m, n)
+
+//37
+// console.log(5 == "5");
+
+//38
+// console.log(5 === "5");
+
+//39
+// console.log(true && false || true);
+
+//40
+5 > 3 ? console.log("Yes") : console.log("No");
+
